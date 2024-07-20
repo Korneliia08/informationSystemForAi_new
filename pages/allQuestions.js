c9e7cea1f7f414283fde451eb4e20579a20326a7
Korneliia <korneliiaPositive@gmail.com>
2024-07-20 15:43:31 +0200

    Initial commit

---
+++ pages/allQuestions.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    question: "Tell me more about GCE",
+    answer: "1",
+  },
+  {
+    question: "Tell me more about CrowdStrike",
+    answer: "2",
+  },
+  {
+    question: "Tell me more about ApexaIQ",
+    answer: "3",
+  },
+  {
+    question: "What’s the latest version of ChatGPT",
+    answer: "4",
+  },
+];
--- pages/chatGPT.js
+++ pages/chatGPT.js
@@ -3,6 +3,9 @@ let windowOfChat = document.querySelector(".windowOfChat");
 let spanClose = document.querySelector(".spanClose");
 
 let searchInputChat = document.querySelector(".searchInputChat");
+let btnSend = document.querySelector(".btnSend");
+
+let questionInInputFieldFromClient = "";
 
 iconOfChat.addEventListener(
   "click",
@@ -13,4 +16,36 @@ spanClose.addEventListener(
   "click",
   () => (windowOfChat.style.display = "none"),
 );
-searchInputChat.addEventListener("input", () => alert("s"));
+
+searchInputChat.addEventListener("input", (event) => {
+  questionInInputFieldFromClient = event.target.value;
+  if (questionInInputFieldFromClient.length !== 0) {
+    btnSend.classList.add("isActive");
+  } else {
+    btnSend.classList.remove("isActive");
+  }
+});
+
+btnSend.addEventListener("click", checkQuestionFromClient);
+
+let isAnswer = false;
+
+function checkQuestionFromClient() {
+  questions.forEach((obj) => {
+    if (obj.question === questionInInputFieldFromClient) {
+      isAnswer = true;
+    } else {
+      isAnswer = false;
+    }
+  });
+  showWindowIfNoAnswer();
+}
+
+function showWindowIfNoAnswer() {
+  let popUpNoAnswer = document.querySelector(".popUpNoAnswer");
+  if (!isAnswer) {
+    setTimeout(() => {
+      popUpNoAnswer.style.display = "flex";
+    }, 2500);
+  }
+}
