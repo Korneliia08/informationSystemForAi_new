23866784e199ea2052cbabdfc14b5cc44554dfcb
AlfaGruisPL <alfagruis@gmail.com>
2024-10-23 12:30:15 +0200

    work

--- scroll.js
+++ scroll.js
@@ -9,11 +9,6 @@ window.scrollTo({
 window.addEventListener(
   "wheel",
   function (e) {
-    if (isScrolling) {
-      e.preventDefault(); // Jeżeli strona już się przewija, nie wykonujemy kolejnej akcji
-      return;
-    }
-
     const scrollPositions = [0, window.innerHeight, window.innerHeight * 2]; // Pozycje 0, 100vh, 200vh
     let currentScroll = window.scrollY; // Aktualna pozycja przewinięcia
     let targetScroll; // Docelowa pozycja do przewinięcia
@@ -23,6 +18,11 @@ window.addEventListener(
       // Sprawdzamy, czy jesteśmy poniżej 200vh, wtedy przewijanie działa normalnie
       if (currentScroll >= scrollPositions[2]) {
         return; // Normalne przewijanie powyżej 200vh
+      }
+
+      if (isScrolling) {
+        e.preventDefault(); // Jeżeli strona już się przewija, nie wykonujemy kolejnej akcji
+        return;
       }
 
       e.preventDefault(); // Zapobiegamy domyślnemu przewijaniu w dół
@@ -56,20 +56,18 @@ window.addEventListener(
         return; // Normalne przewijanie poniżej 200vh
       }
 
-      e.preventDefault(); // Zapobiegamy domyślnemu przewijaniu w górę
-      isScrolling = true; // Blokujemy przewijanie
+      if (isScrolling) {
+        e.preventDefault(); // Jeżeli strona już się przewija, nie wykonujemy kolejnej akcji
+        return;
+      }
 
-      // Sprawdzamy, gdzie jesteśmy i ustawiamy docelową pozycję przewinięcia
-      if (
-        currentScroll > scrollPositions[1] &&
-        currentScroll < scrollPositions[2]
-      ) {
-        targetScroll = scrollPositions[1]; // Przewiń z 200vh do 100vh
-      } else if (
-        currentScroll <= scrollPositions[1] &&
-        currentScroll > scrollPositions[0]
-      ) {
-        targetScroll = scrollPositions[0]; // Przewiń z 100vh do 0vh
+      e.preventDefault(); // Zapobiegamy domyślnemu przewijaniu w górę
+
+      // Sprawdzamy, na której sekcji jesteśmy
+      if (currentScroll > scrollPositions[1] + 50) {
+        targetScroll = scrollPositions[1]; // Przewiń z 200vh do 100vh, tolerancja 50px
+      } else if (currentScroll > scrollPositions[0] + 150) {
+        targetScroll = scrollPositions[0]; // Przewiń z 100vh do 0vh, tolerancja 50px
       }
 
       // Jeżeli ustalono docelową pozycję, przewijamy
